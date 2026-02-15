755409867fb787a2f94836c114a39dd50a5f351c
Emeka Iwuagwu <iwuagwuemmy@yahoo.com>
2026-02-15 20:25:58 +0100

    Fix Prisma constructor for Prisma 7 compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-    const dbUrl = "mysql://thinqmed_root:thinqmedia12345678%2A%2A%2A%40%21@131.153.147.98:3306/thinqmed_thinqmedia_db";
-
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: dbUrl,
-            },
-        },
-    });
+    return new PrismaClient();
 };
 
 declare global {
